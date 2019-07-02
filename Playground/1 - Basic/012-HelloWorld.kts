3baf9c383737ad47520fa2b950a520f333f4c9ce
An. Jorge <an.jorge@icloud.com>
2019-07-02 23:27:28 +0100

    Hello world with parameters

--- Playground/1 - Basic/012-HelloWorld.kts
+++ Playground/1 - Basic/012-HelloWorld.kts
@@ -2,7 +2,31 @@
 
 fun sayHello(){
     println("Hello World")
-    print("I'm loving KOTLIN lang")
+    println("I'm loving KOTLIN lang ")
 }
 
 sayHello()
+
+
+// Hello With parameters
+
+fun sayHello(a: String){
+    println("Hi " + a)  // Other concatenate form
+}
+
+sayHello("Baby")
+
+fun greeting(b: String, c: String){
+    println("Hello! $b and $c")  // Other concatenate form
+}
+
+greeting("Simon", "Ana")
+
+
+// With Default parameters (Value)
+
+fun greeting2(d: String, e: String = "Good morning"){
+    println("$e, $d")
+}
+
+greeting2("Dr. Swift")
