bcf171f396ddc4bb4c0d89ce9170eddb1f3aaa80
An. Jorge <an.jorge@icloud.com>
2019-07-08 10:38:11 +0100

    Double and Float Types

--- Playground/1 - Basic/014-VariablesTypes.kts
+++ Playground/1 - Basic/014-VariablesTypes.kts
@@ -2,8 +2,39 @@ fun variableTypes(){
 
     var name: String = "Maria"
     var age: Int = 23
-    var numb:Float 12.20
-    var numb2: Double 32.23
+    var numb: Float = 121.20f
+    var numb2: Double = 32.23
+
+
+    println(name)
+    println(age)
+    println(numb)
+    println(numb2)
+
+
+    var a = 101
+    var otherA: Int? = 99
+
+    println(a == otherA)
+
+
 }
 
 variableTypes()
+
+
+
+// Double variables
+
+var large: Double? = null
+large = 12.999
+println("Double: $large")
+
+
+
+// Float variables
+
+var short: Float = 23.99f
+short = 345.02f
+
+println("Float number: $short")
