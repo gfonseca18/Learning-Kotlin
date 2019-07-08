7c76b379b3c81b8375927d33deabe7ea26d82666
An. Jorge <an.jorge@icloud.com>
2019-07-08 20:11:22 +0100

    Read user input with readLine Method

---
+++ Playground/1 - Basic/021-readLineMethod.kts
@@ -0,0 +1,20 @@
+println("======================")
+println("Welcome to Jonhy Store")
+println("======================")
+
+println("Hello! What we can to for you? : ")
+var customResponde = readLine()
+println("Yes, indeed. We have fresh bread off the oven! It's your like day")
+var otherResponde = readLine()
+println("Have a nice day!")
+
+
+println("============  ==========")
+println("============  ==========")
+println("Your conversation with Dayse")
+
+println("Dayse: Hello! What we can to for you?")
+println("You: $customResponde")
+println("Dayse: Yes, indeed. We have fresh bread off the oven! It's your like day")
+println("You: $otherResponde")
+println("Dayse: Have a nice day!")
