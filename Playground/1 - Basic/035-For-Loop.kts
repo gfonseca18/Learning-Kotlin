b7f7f7da59e8a5b3440318c8f530dec887f7be20
An. Jorge <an.jorge@icloud.com>
2019-07-11 00:03:16 +0100

    033 Logical Operators AND OR NOT

---
+++ Playground/1 - Basic/035-For-Loop.kts
@@ -0,0 +1,16 @@
+for (i in 1..5) {
+    println("$i Hello World!")
+}
+
+//
+
+println()
+
+
+for (item in 0..20) {
+    if (item % 2 == 0) {
+        println("$item is divisible by 2")
+    } else {
+        println("$item isn't divisible by 2")
+    }
+}
