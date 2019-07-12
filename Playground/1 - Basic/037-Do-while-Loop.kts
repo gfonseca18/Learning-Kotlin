2600eac3d294bcc54bc3ca6cff08cbe4dd93e224
An. Jorge <an.jorge@icloud.com>
2019-07-13 00:13:02 +0100

    training

--- Playground/1 - Basic/037-Do-while-Loop.kts
+++ Playground/1 - Basic/037-Do-while-Loop.kts
@@ -5,4 +5,19 @@ do {
     println("counting... $counter")
 } while (counter < 20)
 
+
 println()
+println()
+
+
+var brothers = 5
+
+do {
+
+    println("How many Brothers I have? ")
+    val responder = readLine()!!.toInt()
+    if (responder == brothers) {
+        println("That's great, you know me")
+    }
+
+} while (responder != 5)
