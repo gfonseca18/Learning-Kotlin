c19faf820a1af218318617ce5c4494156b1c9d3b
An. Jorge <an.jorge@icloud.com>
2019-07-16 23:44:47 +0100

    functions with return types Boolean - update

--- Playground/2 - Medium/041-Functions-Return-Types.kts
+++ Playground/2 - Medium/041-Functions-Return-Types.kts
@@ -6,7 +6,6 @@ println(pers("James", "Bound"))
 
 
 // Other Double
-
 println()
 
 fun mult(numb: Int, numb2: Int) : Double {
@@ -18,7 +17,35 @@ println(mult(9, 90))
 
 
 // Other with Integer
+println()
+
 
 fun integer(a: Int, b: Int) : Int {
-
+    var sum = a + b
+    return sum
 }
+
+println(integer(100, 500))
+
+
+// Boolean
+println()
+
+
+fun bool(sex: Boolean, age: Int) : Boolean {
+    if (sex == true && age >= 18) {
+        println("Welcome man")
+        return true
+    }
+
+    else if (sex == false && age <= 18 || age >= 18) {
+        print("We need men for this job")
+        return false
+
+    } else {
+        println("Too young")
+        return false
+    }
+}
+
+bool(true, 25)
