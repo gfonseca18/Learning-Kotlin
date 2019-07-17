2adda0357c9e2abef9be8c1fb250ad890ecc0e7b
An. Jorge <an.jorge@icloud.com>
2019-07-18 00:10:02 +0100

    Local and Global variables - update

--- Playground/2 - Medium/043-Variable-Scope.kts
+++ Playground/2 - Medium/043-Variable-Scope.kts
@@ -1,2 +1,16 @@
 var country = "Angola"      // Global variable
-fun province(name: String)
+
+fun province(name: String){
+    val greeting = "Welcome to"    // Local variable
+    println("$greeting $name - $country")
+}
+
+province("Uige")
+province("M'banza Congo")
+province("Cabinda")
+province("Cunene")
+
+/*
+    You can't use local variables outside function
+    except global variables
+ */
