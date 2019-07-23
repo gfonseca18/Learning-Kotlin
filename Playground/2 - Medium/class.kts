94ef03415f34318e7b356ea9ba956d8722bf77e9
An. Jorge <an.jorge@icloud.com>
2019-07-23 08:26:51 +0100

    Other class and Object

--- Playground/2 - Medium/class.kts
+++ Playground/2 - Medium/class.kts
@@ -1,4 +1,4 @@
-class House(type: String, price: Double, yearBuild: Int, owner: String){
+class House (type: String, price: Double, yearBuild: Int, owner: String){
     var type: String? = null
     var price: Double? = null
     var yearBuild: Int? = null
@@ -33,4 +33,61 @@ print(
         This is ${place.type} cust ${place.price} Build in ${place.yearBuild}, 
         propriet of ${place.owner}
 """
+)
+
+
+println("====== ======== ======= ===== ====")
+
+
+
+// Other Class
+
+
+
+class Coin (name: String, value: Int, color: String, size: Double){
+    var name: String? = null
+    var value: Int? = null
+    var color: String? = null
+    var size: Double? = null
+
+    init {
+        this.name = name
+        this.value = value
+        this.color = color
+        this.size = size
+    }
+}
+
+var kz_50 = Coin("Kwanza", 50, "Prateado", 5.5)
+
+println(
+        """
+                Angola Coin
+            -------------------
+            Name: ${kz_50.name} 
+            Value: ${kz_50.value} Kz
+            Color: ${kz_50.color}
+            Size: ${kz_50.size} mm
+
+        """.trimIndent()
+)
+
+var kz_100 = kz_50
+kz_100.name = "Real"
+kz_100.value = 1
+kz_100.color = "Brown"
+kz_100.size = 2.0
+
+
+
+println(
+        """
+                Brasil Coin
+            -------------------
+            
+            Name: ${kz_100.name}
+            Value: ${kz_100.value} R$
+            Color: ${kz_100.color}
+            Size: ${kz_100.size} mm
+        """.trimIndent()
 )
