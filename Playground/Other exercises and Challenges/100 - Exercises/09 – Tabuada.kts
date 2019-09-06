b82caa5bcc4c53fde56c36d2c4570663ce0fa0a9
An. Jorge <an.jorge@icloud.com>
2019-09-06 16:16:25 +0100

    Update 09 – Tabuada.kts
    09 - completed

--- Playground/Other exercises and Challenges/100 - Exercises/09 – Tabuada.kts
+++ Playground/Other exercises and Challenges/100 - Exercises/09 – Tabuada.kts
@@ -3,3 +3,12 @@
 Exercício Python 009: Faça um programa que leia um número Inteiro qualquer e mostre na tela a sua tabuada.
 
 */
+
+var tab = 3
+
+fun main() {
+    println("Tabuada de $tab")
+    for (i in 0..12) {
+        println("$tab * $i = ${tab * i}")
+    }
+}
