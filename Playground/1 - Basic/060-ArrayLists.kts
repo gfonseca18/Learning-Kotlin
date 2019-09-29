f3da2a36e0819a27c01ceec9dab96799bb02c8a5
An. Jorge <an.jorge@icloud.com>
2019-09-29 22:58:37 +0100

    Array Lists - Begin

---
+++ Playground/1 - Basic/060-ArrayLists.kts
@@ -0,0 +1,19 @@
+
+// Array Lists is like mostly array in another languages (swift, Python)
+/*
+
+The Big differece between Array and ArrayList is in ArrayList you can print
+elements without method for loop, and no don't need to specify the size
+
+ */
+
+
+var a = ArrayList<String>()
+// Add elements
+a.add("A")
+a.add("B")
+a.add("Paul")
+
+println("${a.get(2)} has Index 2") // Print element by Index
+println(a)
+
