cae6441832e03170df4fba730db93446c26dbeb0
an-jorge <an.jorge@icloud.com>
2020-11-14 14:13:51 +0100

    More about ArrayList

--- Playground/1 - Basic/060-ArrayLists.kts
+++ Playground/1 - Basic/060-ArrayLists.kts
@@ -8,12 +8,46 @@ elements without method for loop, and no don't need to specify the size
  */
 
 
-var a = ArrayList<String>()
+var lista = ArrayList<String>()
 // Add elements
-a.add("A")
-a.add("B")
-a.add("Paul")
+lista.add("Alibaba")
+lista.add("Benguela")
+lista.add("Paul")
+lista.add("Luanda")
 
-println("${a.get(2)} has Index 2") // Print element by Index
-println(a)
+println("${lista.get(2)} has Index 2") // Print element by Index
+println(lista)
 
+// Removing elements
+lista.remove("Paul")
+println(lista)
+
+//
+
+
+println()
+        println("BLANK SPACE")
+println()
+
+// MUTABLES LIST
+
+val movies = mutableListOf<String>()
+movies.add("Matrix")
+movies.add("John Rambo")
+movies.add("Exterminator 2")
+movies.add("The last of Us")
+movies.add("X-Man")
+println(movies)
+
+movies.first()
+movies.last()
+movies.size
+movies.slice(1..3 ) // only get elements from 1 to 3
+movies.contains("X-Man")
+movies.sort()
+movies.joinToString(":")
+
+
+for ((index, name) in movies.withIndex()) {
+        println("$index, $name") // Print list with index
+}
