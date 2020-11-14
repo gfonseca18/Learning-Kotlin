0d5b003bf6e3d7d1acdd2dfa85b4157fd4b7ae6d
an-jorge <an.jorge@icloud.com>
2020-11-14 23:17:13 +0100

    Function lambda - Unit

--- Playground/2 - Medium/040-Fun-Lambda.kts
+++ Playground/2 - Medium/040-Fun-Lambda.kts
@@ -12,3 +12,13 @@ var calculate = { a: Int, b: Int -> Int
 
 var result2 = calculate(30, 30)
 println(result2)
+
+//
+
+var lambda: () -> Unit = {
+    println("Hello World")
+
+    // Lambda function without return, that's why is Unit
+}
+
+lambda()
