77cde1e81611d8fdaee19f80068b61583e1895d4
an-jorge <an.jorge@icloud.com>
2020-11-19 09:11:46 +0100

    File Renamed

--- Playground/Other exercises and Challenges/100 - Exercises/12 – Calculando Descontos.kts
+++ Playground/Other exercises and Challenges/100 - Exercises/12 – Calculando Descontos.kts
@@ -3,13 +3,16 @@
 Exercício Python 012: Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
 
 */
+
 fun main() {
-    var article = 100
-    var discount = 50
-    var sum = article * discount / 100
+    val article = 100
+    val discount = 50
+    val sum = article * discount / 100
     println(
         """
         The article cost ${article} kz with ${discount} off
         Value of discount is ${sum} kz.
         """)
 }
+
+main()
