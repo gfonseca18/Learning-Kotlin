8d252f193bc4009773bfebcef4728359de3553db
an-jorge <an.jorge@icloud.com>
2020-11-29 12:00:48 +0100

    Review 24 – Procurando String _SUCCESS

--- Playground/Other exercises and Challenges/100 - Exercises/24 – Primeiras letras de um texto.kts
+++ Playground/Other exercises and Challenges/100 - Exercises/24 – Primeiras letras de um texto.kts
@@ -1,13 +1,16 @@
 /*
-24 – Primeiras letras de um texto
-Exercício Python 024: Crie um programa que leia o nome de uma cidade diga se ela começa ou não com o nome "SANTO".
+
+25 – Procurando String
+Exercício Python 025:
+Crie um programa que leia o nome de uma pessoa e diga se ela tem "SILVA" no nome.
+
 */
 
-println("Digite o nome para verificar se contém o nome ''Santo'' ")
-fun main() {
+println("Digite o nome para verificar se contém o nome ''SILVA'' ")
 
-    var name = readLine()!!.toString().toUpperCase()
-    if (name.contains("SANTO")) {
+fun main() {
+    val name = readLine()!!.toString().toUpperCase()
+    if (name.contains("SILVA")) {
         println("Bingo")
     } else {
         println("Nop")
