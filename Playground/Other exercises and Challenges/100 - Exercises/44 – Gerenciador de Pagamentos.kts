64a2fac1e381f27a72638799fb986f754b407a68
an-jorge <an.jorge@icloud.com>
2021-05-06 06:02:44 +0100

    44 – Gerenciador de Pagamentos _SUCCESS

--- Playground/Other exercises and Challenges/100 - Exercises/44 – Gerenciador de Pagamentos.kts
+++ Playground/Other exercises and Challenges/100 - Exercises/44 – Gerenciador de Pagamentos.kts
@@ -11,8 +11,12 @@ considerando o seu preço normal e condição de pagamento:
 - 3x ou mais no cartão: 20% de juros
 
  */
-val ipadPro = 1000
+
+println("Digite o preço final da compra: ")
+val purchase = readLine()!!.toInt()
+
 fun main() {
+
     println("–+ Gerenciador de Pagamentos –+ \n")
     println("\n" +
             "[1] - à vista dinheiro/cheque: 10% de desconto \n" +
@@ -21,13 +25,9 @@ fun main() {
             "[4] - 3x ou mais no cartão: 20% de juros ")
 
 
+
     println(" - Sua formas de pagamentos - ")
     val payment = readLine()!!.toInt()
-
-   option_1()
-   // option_2()
-   // option_3()
-    option_4()
 
     when (payment) {
         1 -> println(option_1())
@@ -40,29 +40,34 @@ fun main() {
 
 }
 
+
 fun option_1(discount: Int = 10) : String {
-    val sale = ipadPro * discount / 100
-    return "iPad Pro custando ${ipadPro} kz \n " +
+    val sale = purchase - (purchase * discount / 100)
+    return "Compras no valor de ${purchase} kz \n " +
         "Opçāo [1]: - à vista dinheiro/cheque - \n" +
             " Preço final com 10% de desconto ${sale}"
 }
 
 fun option_2(discount: Int = 5) : String {
-    val sale = ipadPro * discount / 100
-    return "iPad Pro custando ${ipadPro} kz \n " +
-            "Opçāo [2]: - à vista no cartão - \n" +
-            " Preço final com 5% de desconto ${sale}"
+    val sale = purchase - (purchase * discount / 100)
+    return "Compras no valor de ${purchase} kz \n " +
+            "Opçāo escolhida Opçāo [2]: - à vista no cartão - \n" +
+            " Preço final com 5% de desconto ${sale} Kz"
 }
 
 fun option_3() : String {
-    return "iPad Pro custando ${ipadPro} kz \n " +
-            "Opçāo [3]: - em até 2x no cartão - \n" +
-            " preço formal sem desconto"
+    val sales = purchase / 2
+    return "Compras no valor de ${purchase} kz \n " +
+            "Opçāo escolhida [3]: - em até 2x no cartão - \n" +
+            "2x de $sales kz preço final $purchase Kz sem desconto"
 }
 
 fun option_4() : String {
-    return "iPad Pro custando ${ipadPro} kz \n " +
-            "Opçāo [4]: - 3x ou mais no cartão 20% de juros - "
+    val sales = purchase + (purchase * 20 / 100)
+    return "Compras no valor de ${purchase} kz \n " +
+            "Opçāo escolhida [4]: - 3x ou mais no cartão 20% de juros - \n" +
+            "Preço final $sales"
 }
+
 
 main()
