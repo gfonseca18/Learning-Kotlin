edca0ab4a2411588813582343b1c33efd61e0323
an-jorge <an.jorge@icloud.com>
2021-07-13 00:07:33 +0100

    54 – Grupo da Maioridade _SUCCESS [] - Refactor

--- Playground/Challenges/001 - Exercises/54 – Grupo da Maioridade.kts
+++ Playground/Challenges/001 - Exercises/54 – Grupo da Maioridade.kts
@@ -2,16 +2,38 @@
 
 54 – Grupo da Maioridade
 Exercício Python 054:
-Crie um programa que leia o ano de nascimento de sete pessoas.
-No final, mostre quantas pessoas ainda não atingiram a maioridade e
-quantas já são maiores.
+[x] Crie um programa que leia o ano de nascimento de sete pessoas.
+[x] No final, mostre quantas pessoas ainda não atingiram a maioridade e
+[x] quantas já são maiores.
+
  */
 
+// - [] Refactor
+
 fun main() {
-   // val current = 2021
-    val peopleCount =
-    while (true) {
+    var peopleList = mutableListOf<Int>()
+    var youngList = mutableListOf<Int>()
+    var adultList = mutableListOf<Int>()
+
+    while (peopleList.count() < 3) {
         println("Em que ano você nasceu? >: ")
-        val peopleCount =
+        var peopleCount = readLine()!!.toInt()
+        peopleList.add(peopleCount)
+
     }
+    for (item in peopleList) {
+        if (2021 - item <= 17) {
+            youngList.add(item)
+        } else {
+            adultList.add(item)
+        }
+    }
+
+
+    println("GERAL $peopleList")
+    println("Têm ${youngList.size} Menores")
+    println("Têm ${adultList.count()} Maiores")
+
 }
+
+main()
