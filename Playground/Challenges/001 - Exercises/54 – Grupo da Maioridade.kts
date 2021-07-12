85ba64c0aa8dca21b080ba8c1cc2bea3f6b4499c
an-jorge <an.jorge@icloud.com>
2021-07-12 22:14:13 +0100

    commit Start

---
+++ Playground/Challenges/001 - Exercises/54 – Grupo da Maioridade.kts
@@ -0,0 +1,17 @@
+/*
+
+54 – Grupo da Maioridade
+Exercício Python 054:
+Crie um programa que leia o ano de nascimento de sete pessoas.
+No final, mostre quantas pessoas ainda não atingiram a maioridade e
+quantas já são maiores.
+ */
+
+fun main() {
+   // val current = 2021
+    val peopleCount =
+    while (true) {
+        println("Em que ano você nasceu? >: ")
+        val peopleCount =
+    }
+}
