117d14af6bdf98ae8a1d5efc8c18b782f8f31e60
An. Jorge <an.jorge@icloud.com>
2021-08-02 10:31:32 +0100

    deleted Achived folder

--- 2-Challenges/Achieved/Market/classes/Products.kt
+++
@@ -1,8 +0,0 @@
-package classes
-
-class Products (val name: String, val price: Double) {
-
-    fun showProductsData() : String {
-        return "Nome de produto: $name, valor Kz $price"
-    }
-}
