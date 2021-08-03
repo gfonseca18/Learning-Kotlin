8abd5ce56c7300ba464c7967644724090e389c58
An. Jorge <an.jorge@icloud.com>
2021-08-03 09:51:53 +0100

    * Re-factory

--- 2-Challenges/001 - Exercises/09 – Tabuada.kts
+++ 2-Challenges/001 - Exercises/09 – Tabuada.kts
@@ -1,14 +1,18 @@
 /*
 09 – Tabuada
-Exercício Python 009: Faça um programa que leia um número Inteiro qualquer e mostre na tela a sua tabuada.
+Exercício Python 009:
+Faça um programa que leia um número Inteiro qualquer e mostre na tela a sua tabuada.
 
 */
 
-var tab = 3
-
 fun main() {
-    println("Tabuada de $tab")
-    for (i in 0..12) {
-        println("$tab * $i = ${tab * i}")
+
+    val tabuada = 10
+    println("== == Tabuada de $tabuada == ==")
+    println()
+    for (index in 0..12) {
+        println("$tabuada * $index = ${tabuada * index}")
     }
 }
+
+main()
