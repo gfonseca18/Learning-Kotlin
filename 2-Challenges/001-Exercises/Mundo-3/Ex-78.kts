3ec8677d21989dfc7ee5064e313e02008923edf2
An. Jorge <an.jorge@icloud.com>
2021-08-03 20:50:59 +0100

    Re-org files names

---
+++ 2-Challenges/001-Exercises/Mundo-3/Ex-78.kts
@@ -0,0 +1,21 @@
+/*
+
+Exercício Python 078:
+Faça um programa que leia 5 valores numéricos [x]
+e guarde-os em uma lista.[x]
+No final, mostre qual foi o maior e o menor valor digitado e as suas respectivas posições na lista. [x]
+
+*/
+
+fun main() {
+
+   val list = mutableListOf<Int>()
+   repeat(5) {
+      println("Digite o ${it +1}º número >: ")
+      val number = readLine()!!.toInt()
+      list.add(number)
+   }
+   println("<: Maior número digitado: ${list.maxOrNull()} \n Menor número digitado: ${list.minOrNull()}")
+}
+
+main()
