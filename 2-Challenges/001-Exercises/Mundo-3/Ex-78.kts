fa520fefae5f2ee442d08e0bdfb5c77a536a157d
An. Jorge <an.jorge@icloud.com>
2021-08-06 19:48:55 +0100

    * Docs

--- 2-Challenges/001-Exercises/Mundo-3/Ex-78.kts
+++ 2-Challenges/001-Exercises/Mundo-3/Ex-78.kts
@@ -10,7 +10,6 @@ No final, mostre qual foi o maior e o menor valor digitado e as suas respectivas
 fun main() {
 
    val list = mutableListOf<Int>()
-   rep
       println("Digite o ${it +1}º número >: ")
       val number = readLine()!!.toInt()
       list.add(number)
